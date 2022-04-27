ba0f9fc93b00a2a460144517e0e7770911ea17af
theonetruepatrick <104328646+theonetruepatrick@users.noreply.github.com>
2022-04-26 22:47:20 -0400

    Update WaterLevelSensor.ino

--- WaterLevelSensor/WaterLevelSensor.ino
+++ WaterLevelSensor/WaterLevelSensor.ino
@@ -8,7 +8,7 @@
  * 
  * sensor anomolies:
  *     - mechanical minimum read of sensor is 9".  Sensor limitation can cause tank overflow.
- *     - sensor has 50% chance of reading "0".  not sure why
+ *     - sensor has chance of reading "0".  not sure why
  *     
  *  Process: 
  *    1) Will read level every minute;
@@ -27,71 +27,188 @@
  */
 
   // Define Trig and Echo pin:
-    #define trigPin 2   // for ESP8266 - physical pin D4
-    #define echoPin 15  // for ESP8266 - physical pin D8
+    #define trigPin 2   // for ESP8266 - physical pin D4 connected to JSN-SR04T "dx trig" pin
+    #define echoPin 15  // for ESP8266 - physical pin D8 connected to JSN-SR04T "dx echo" pin
   
   // Define variables for sensor:
-    float duration;       //Sensor pulse 
-    float distance;       //Sensor pulse
-    float tankFull = 10;  //minimum distance (inches) between sensor and water level aka FULL
-    float tankEmpty = 21; //maximum distance (inches) between sensor and water level aka EMPTY
-    float tankAlarm = 30; //Threshold for alerting if level falls below this %
-    float tankLevel = 0;  //the % of the tank level
+    float duration;               //Sensor pulse
+    float distance;               //Sensor pulse
+    float tankLevel;              //the % of the tank level
+  
+  //Variables to set specific to each tank
+    float tankFull = 10;          //minimum distance (inches) between sensor and water level aka FULL
+    float tankEmpty = 21;         //maximum distance (inches) between sensor and water level aka EMPTY
+    float tankLevelMax = 100;     //highest value (%) allowed for tank level
+    float tankLevelMin = 0;       //lowest value (%) allowed for tank level
+    float distanceOffset = 1.0;   //compensates for reading in INCHES - not sure if this is common across all sensors
+    float tankAlarm = 30;         //Threshold for alerting if level falls below this %
+    
+    const float speedOfSound = 0.0135;  //speed of sound: inches per MICROsecond
+    float durationTimeout = (tankEmpty*1.5*2)/speedOfSound;  //used to shorten wait time for pulse 
   
   // Define variables for smoothing array:
-    const int numReadings = 30;
-    int readings[numReadings];      
-    int readIndex = 0;
+    int readingTimestamp = 0;    //timestamp [milliseconds] variable for reading cycle
+    int readingDelayMS = 60000;  //time gap between readings; default is 60 seconds
+    int readingDelayAdjustment;  //me being WAY too anal retentive
+    const int numReadings = 15;  //number of readings to be averaged/smoothed
+    int readingCount=0;          //current total number of readings
+    int readings[numReadings];   //array that stores readings
+    int readIndex = 0;           
+
+    float readingSumTotal = 0;
+    float readingAverage = 0;
+
+  //Optional: Display uptime in human friendly format
+    unsigned long constSeconds = 1000;
+    unsigned long constMinutes = 60000;
+    unsigned long constHours = 3600000;
+    unsigned long constDays = 86400000;
+    int tsSeconds = 0;
+    int tsMinutes = 0;
+    int tsHours = 0;
+    int tsDays = 0;
+    
+    String tsUptime="";
+  
+
   
 void setup() {
-  // Begin Serial communication at a baudrate of 9600:
     Serial.begin(115200);
-  // Define inputs and outputs
-    pinMode(trigPin, OUTPUT);
-    pinMode(echoPin, INPUT);
+    Serial.println();
+    Serial.println ("Booting Up.....");
   
-  // initialize all the readings to 0:
-    for (int thisReading = 0; thisReading < numReadings; thisReading++) {
-      readings[thisReading] = 0;
-    }
-
+  // Define inputs and outputs
+    pinMode(trigPin, OUTPUT);  //JSN-SR04T: dx trig
+    pinMode(echoPin, INPUT);   //JSN-SR04T: tx echo 
+ 
+  
 }
 
 void loop() {
   // Clear the trigPin by setting it LOW:
   digitalWrite(trigPin, LOW);
   
-  delay(1000);
+  if (millis()-readingTimestamp >= readingDelayMS){  // will execute only after pre-defined time period
+    
+    getReading();
 
- // Trigger the sensor by setting the trigPin high for 10 microseconds:
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(500);
-  digitalWrite(trigPin, LOW);
-  
-  // Read the echoPin. pulseIn() returns the duration (length of the pulse) in microseconds:
-  duration = pulseIn(echoPin, HIGH);
-  
-  // Calculate the distance:
-  distance = duration*0.034/2;  //converts the duration to CM
-  distance = distance/2.54;     //converts CM to IN
-  distance = distance+1;        //adds constant error correction
-
-  tankLevel = round(100*(1-((distance-tankFull)/(tankEmpty-tankFull))));
-  
-  
-  
-  
-  // Print the distance on the Serial Monitor (Ctrl+Shift+M):
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.print (" in : Tank Fill level: ");
-  Serial.print(tankLevel);
-  Serial.print(" %");
-  if (tankLevel<=tankAlarm){
-    Serial.print ("  !!!ALERT!!!    Low Tank Level    !!!ALERT!!!");
   }
-  Serial.println();
   
+}
+
+void getReading(){
+   // Trigger the sensor by setting the trigPin high for 10 microseconds:
+      digitalWrite(trigPin, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin, LOW);  //
+   
+      duration = pulseIn(echoPin, HIGH, durationTimeout); // Read the echoPin for round-trip pulse time.
+
+      if (duration > 10){  //sometimes echo fails; returns zero
+        readingTimestamp=millis();    //timestamp this loop
+        readingDelayAdjustment= readingTimestamp % readingDelayMS;
+        readingTimestamp -= readingDelayAdjustment;
+     
+       // Calculate the distance 
+          distance = duration*speedOfSound;     //converts the duration to INCHES
+          distance /= 2;                //halves distance for round trip
+          distance += distanceOffset; //adds compensation to measurement
+       
+      // converts distance to % of tank    
+          tankLevel = round(100*(1-((distance-tankFull)/(tankEmpty-tankFull))));
+          tankLevel = max(tankLevel, tankLevelMin);   //doesn't allow reading to go below 0
+          tankLevel = min(tankLevel, tankLevelMax);   //doesn't allow reading to go above 100
+      
+      // Call the smoothing formula
+          smoothData();
+          
+      // display output
+          serialMonitorOutput();
+       
+     } else {
+        readingTimestamp=millis()-readingDelayMS;     //if distance returns a "0", re-run reading
+        Serial.print (readingTimestamp);
+        Serial.println(" : Error: zero value returned, redoing reading....");
+     }
+      
+}
+
+void serialMonitorOutput(){   //Displays the information to Serial Monitor
+      
+      millisToHuman();
+      Serial.print ("Uptime (D:H:M:S): ");
+       Serial.println (tsUptime);
+      Serial.print ("milliseconds: ");
+       Serial.println (readingTimestamp);
+      if (tankLevel<=tankAlarm){
+        Serial.println ("  !!!ALERT!!!    Low Tank Level    !!!ALERT!!!");
+      }
+      Serial.print ("     Pulse duration (round trip) ");
+      Serial.println (duration);
+      
+      Serial.print("     Distance to liquid surface: ");
+      Serial.print(distance);
+      Serial.println (" inch");
+     
+      Serial.print ("     Current tank reading: ");
+      Serial.print(tankLevel);
+      Serial.println("%");
+
+      Serial.print ("     Average of tank reading: ");
+      Serial.print(readingAverage);
+      Serial.print("%");
+      if (readingCount < numReadings) {
+        Serial.print("**");     //visual flag to indicate that current average is not a full set of data yet
+      }
+      Serial.println();
+      Serial.println();
+}
+
+void smoothData(){
+    readingSumTotal -= readings[readIndex]; // subtract the last reading from the total
+    readings[readIndex] = tankLevel;        // assigns current level to array
+    readingSumTotal += readings[readIndex]; // add the currentreading to the total
+    
+    readIndex = readIndex + 1;              // advance to the next position in the array:
+    if (readIndex >= numReadings) {         // if at the end of the array, wrap around to the beginning
+      readIndex = 0;
+    }
+    if (readingCount < numReadings){        //accomodates initial lack of data until array is fully populated 
+      readingCount++;
+      readingAverage = readingSumTotal / readingCount;
+                                          
+    } else {
+      readingAverage = readingSumTotal / numReadings;
+    }
+}
+
+void reboot() {
+  wdt_disable();
+  wdt_enable(WDTO_15MS);
+  while (1) {}
+}
+
+void millisToHuman(){
+  tsUptime="";
   
+  tsDays=readingTimestamp/constDays;
+  tsHours = (readingTimestamp%constDays)/constHours;
+  tsMinutes = ((readingTimestamp%constDays)%constHours)/constMinutes;
+  tsSeconds = (((readingTimestamp%constDays)%constHours)%constMinutes)/constSeconds;
+
+  if (tsDays<10){tsUptime+="0";}
+  tsUptime +=String(tsDays);
+  tsUptime +=":";
+  
+    if (tsHours<10){tsUptime+="0";}
+    tsUptime +=String(tsHours);
+    tsUptime +=":";
+  
+      if (tsMinutes<10){tsUptime+="0";}
+      tsUptime +=String(tsMinutes);
+      tsUptime +=":";
+  
+        if (tsSeconds<10){tsUptime+="0";}
+        tsUptime +=String(tsSeconds);
   
 }
